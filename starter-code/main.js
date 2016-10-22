1e3f8f15b26ce953816f59aafa93ad5a113c47ab
James Ferguson <Ferg@Jamess-MacBook-Air.local>
2016-10-22 11:17:41 +1100

    created js cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,20 @@ var cardOne = "Queen";
 var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
+
+// if (cardOne === cardTwo){
+//   alert('You found a match!');
+// }else {
+//  alert('Sorry, try again.');
+// } 
+
+var board = document.getElementById('game-board');
+
+var createCards = function() {
+for (var i=0; i<cards.length; i++){
+var cardElement = document.createElement('div');
+cardElement.className = 'card';
+board.appendChild(cardElement);
+}
+
+}
